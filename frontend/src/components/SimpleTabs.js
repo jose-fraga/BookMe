fb5b94f7fa142a6fd2fd9cad4b25ace277cc253f
jose fraga <josemendezfraga@gmail.com>
2021-08-05 10:21:11 -0400

    chatroom messages css

--- frontend/src/components/SimpleTabs.js
+++ frontend/src/components/SimpleTabs.js
@@ -147,7 +147,7 @@ export default function PrimarySearchAppBar(props) {
             <img className="Logo" src={img} />
           </IconButton>
           <Typography className={classes.title} variant="h6" noWrap>
-            BookMe
+            Hello
           </Typography>
           <div className={classes.search}>
             <div className={classes.searchIcon}>
